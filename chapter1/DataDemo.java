dc0a308c4d763d6b35927e7ecea95a113f7c7492
GioNik7 <40452568+GioNik7@users.noreply.github.com>
2020-06-03 00:45:44 +0300

    chapter 1 java programms

---
+++ chapter1/DataDemo.java
@@ -0,0 +1,23 @@
+/*
+ *Copyright.....
+ */
+
+package gr.aueb.elearn.ch1;
+
+/**
+ * υπολογιζει και εμφανιζει το αθροισμα 2 αριθμων του 12 και του 5
+ */
+public class DataDemo {
+    public static void main(String[] args) {
+        //Δηλωση αρχικοποιηση μεταβλητων
+        int num1 = 12;
+        int num2 = 5;
+        int sum;
+
+        //Εντολες
+        sum = num1 + num2;
+
+        //Εκτυπωση αποτελεσματων
+        System.out.println("Το αθροισμα ειναι" + sum);
+    }
+}
---
+++ chapter1/HelloWorld.java
@@ -0,0 +1,13 @@
+package gr.aueb.elearn.ch1;
+
+/**
+ * prints Hello World
+ * @author nikolett
+ * @version 0.1
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
